28b85d26338ce5c0d36c6d2d610ca5569e013d6e
Ye Tun Thu <yetunthu@example.com>
2025-03-28 05:58:59 +0800

    policy-template-update

--- policyascode/opa_policy_template.tf
+++ policyascode/opa_policy_template.tf
@@ -1,15 +1,15 @@
-resource "kubernetes_manifest" "logsensitivity_constraint_template" {
+resource "kubernetes_manifest" "logsensitivity_v2_constraint_template" {
   manifest = {
     "apiVersion" = "templates.gatekeeper.sh/v1beta1"
     "kind"       = "ConstraintTemplate"
     "metadata" = {
-      "name" = "logsensitivity"
+      "name" = "logsensitivity-v2"  # Renamed to avoid conflict
     }
     "spec" = {
       "crd" = {
         "spec" = {
           "names" = {
-            "kind" = "LogSensitivity"
+            "kind" = "LogSensitivityV2"  # Changed CRD kind to avoid duplication
           }
         }
       }
@@ -23,7 +23,21 @@ resource "kubernetes_manifest" "logsensitivity_constraint_template" {
               input.review.object.kind == "Pod"
               input.review.object.spec.containers[_].env[_].name == "LOG_FIELDS"
               contains_sensitive_data(input.review.object.spec.containers[_].env[_].value)
-              msg := "Log contains sensitive data"
+              msg := "Pod contains sensitive log data (email, credit card, etc.)"
+            }
+
+            violation[{"msg": msg}] {
+              input.review.object.kind == "ConfigMap"
+              input.review.object.metadata.labels["app.kubernetes.io/name"] == "grafana"
+              input.review.object.data["public"] == "true"
+              msg := "Grafana dashboard should not be publicly accessible"
+            }
+
+            violation[{"msg": msg}] {
+              input.review.object.kind == "ConfigMap"
+              input.review.object.metadata.labels["app.kubernetes.io/name"] == "prometheus"
+              contains_sensitive_data(input.review.object.data["metrics"])
+              msg := "Prometheus metrics contain sensitive data"
             }
 
             contains_sensitive_data(log_fields) {
@@ -32,6 +46,10 @@ resource "kubernetes_manifest" "logsensitivity_constraint_template" {
 
             contains_sensitive_data(log_fields) {
               log_fields[_] == "credit_card"
+            }
+
+            contains_sensitive_data(log_fields) {
+              log_fields[_] == "ssn"
             }
           EOT
         }
